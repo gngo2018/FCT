c30a2dce6e4a594af307f3a1bfe6fccede0e4646
gngo2018 <gngo2018@gmail.com>
2018-11-28 10:16:40 -0500

    Working DB

---
+++ CharacterThrowdown.Data/Character.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharacterThrowdown.Data
+{
+    public class Character
+    {
+        [Key]
+        public int CharacterId { get; set; }
+
+        [Required]
+        public string CharacterName { get; set; }
+
+        [Required]
+        public string CharacterUniverse { get; set; }
+
+        [Required]
+        public string CharacterAbillity { get; set; }
+    }
+}
