5c786fe1485c00972ca28da4257b1bee066d9fc5
gngo2018 <gngo2018@gmail.com>
2018-11-28 09:51:21 -0500

    Add project files.

---
+++ CharacterThrowDown.WebMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CharacterThrowDown.WebMVC.Startup))]
+namespace CharacterThrowDown.WebMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
